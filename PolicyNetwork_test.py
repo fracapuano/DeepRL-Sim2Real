20cf48d958691f12089cb18bbe5144f470859658
Francesco Capuano <francesco_capuano@aol.com>
2022-04-28 00:13:25 +0200

    slight parameters exharcebation

--- PolicyNetwork_test.py
+++ PolicyNetwork_test.py
@@ -1,14 +1,14 @@
 from agent import *
 from pprint import pprint
 
-layers = 4
+layers = 100
 
 params = {
     "state_space": 11, 
     "action_space": 3, 
     "hidden_layers": layers, 
     "hidden_neurons": 32 * np.ones(layers, dtype = np.int16), 
-    "activation_function": np.array([nn.Tanh for _ in range(layers)]), 
+    "activation_function": np.array([nn.ReLU for _ in range(layers)]), 
     "output_activation": nn.Softmax,
     "init_sigma": 0.5
 }
