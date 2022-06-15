9686ccf72ed7fa8c568cf8eaf0d322563c2e1d20
fracapuano <francesco_capuano@aol.com>
2022-06-15 16:30:52 +0200

    step4 tests: test conduced to find the best configuration for the best bound coming out of bayRN

--- step4_tests/tests_loader_trpo.py
+++ step4_tests/tests_loader_trpo.py
@@ -16,7 +16,7 @@ from sb3_contrib.trpo.trpo import TRPO
 from adaptive_dr.BayRn import get_bc
 
 print("Optimizing distrubtions' bounds with BayRn...")
-bounds = get_bc(verbose=1)
+bounds = get_bc(verbose=0)
 print(f"Using bounds: {bounds}")
 
 with open("trpo/trpo.txt", "r") as trpof:
@@ -61,11 +61,11 @@ with open("trpo/trpo_evaluation.txt", "w") as trpo_evaluation_f:
 
         agent.learn(total_timesteps=config['timesteps'])
         saveModel.save_model(agent=agent, agent_type='trpo', folder_path='./')
-        ss_return, _ = testModel.test(agent, agent_type='trpo', env=source_env, episodes=50, model_info='./trpo-model.mdl', render_bool=False)
-        ss_return, _ = testModel.test(agent, agent_type='trpo', env=target_env, episodes=50, model_info='./trpo-model.mdl', render_bool=False)
+        ss_return = testModel.test(agent, agent_type='trpo', env=source_env, episodes=50, model_info='./trpo-model.mdl', render_bool=False)
+        st_return = testModel.test(agent, agent_type='trpo', env=target_env, episodes=50, model_info='./trpo-model.mdl', render_bool=False)
 
         del agent
         os.remove('trpo-model.mdl')
 
-        trpo_evaluation_f.write(f"{i},{ss_return},{ss_return}"+'\n')
+        trpo_evaluation_f.write(f"{i},{ss_return},{st_return}"+'\n')
 trpo_evaluation_f.close()
