baa6382dcad3e21fa5c2042c7d20be7db2ff296a
Francesco Capuano <francesco_capuano@aol.com>
2022-06-27 14:00:04 +0200

    reinforce sumup final version

--- data_analysis/reinforce_sumup/sumup.py
+++ data_analysis/reinforce_sumup/sumup.py
@@ -19,28 +19,41 @@ seedless_df_baseline_actions = pd.read_csv(f"seedless_{ALGS[0]}_actions.txt")
 seedless_df_standard_actions = pd.read_csv(f"seedless_{ALGS[1]}_actions.txt")
 seedless_df_togo_actions = pd.read_csv(f"seedless_{ALGS[2]}_actions.txt")
 
-fig1 = plt.figure(figsize=(24,12))
+fig1, ax = plt.subplots(figsize = (8, 6), nrows = 3)
 
-plot = sns.lineplot(x=seedless_df_baseline_rewards.iloc[:, 0], y=seedless_df_baseline_rewards.iloc[:, 1], data=seedless_df_baseline_rewards)
-plot = sns.lineplot(x=seedless_df_standard_rewards.iloc[:, 0], y=seedless_df_standard_rewards.iloc[:, 1], data=seedless_df_standard_rewards)
-plot = sns.lineplot(x=seedless_df_togo_rewards.iloc[:, 0], y=seedless_df_togo_rewards.iloc[:, 1], data=seedless_df_togo_rewards)
+freq = 250
 
-plot.set_xscale("log")
-plot.set_xlabel("Episode", fontsize=12)
-plot.set_ylabel("Episode Return", fontsize=12)
-plot.set_title("Return per episode for REINFORCE with three different return implementations", fontsize=12, fontweight="bold")
-plot.lines[0].set_linestyle("--")
-plot.lines[0].set_label("REINFORCE with Baseline")
-plot.lines[1].set_linestyle("dotted")
-plot.lines[1].set_label("REINFORCE Standard")
-plot.lines[2].set_linestyle("dashdot")
-plot.lines[2].set_label("REINFORCE with reward To-Go")
-plot.collections[0].set_label(None)
-plot.collections[1].set_label(None)
-plot.collections[2].set_label(None)
-plt.legend()
-plt.savefig("return_per_episode_reinforces.svg", format="svg")
-plt.close(fig1)
+plot = sns.lineplot(x = seedless_df_standard_rewards.iloc[:, 0][::freq], 
+                    y = seedless_df_standard_rewards.iloc[:,1][::freq], 
+                    data = seedless_df_standard_rewards, ci = "sd",
+                    ax = ax[0], linewidth = 0.5)
+plot = sns.lineplot(x = seedless_df_togo_rewards.iloc[:, 0][::freq],
+                    y = seedless_df_togo_rewards.iloc[:,1][::freq],
+                    data = seedless_df_togo_rewards, ci = "sd",
+                    ax = ax[1], linewidth = 0.5)
+plot = sns.lineplot(x = seedless_df_baseline_rewards.iloc[:, 0][::freq],
+                    y = seedless_df_baseline_rewards.iloc[:,1][::freq],
+                    data = seedless_df_baseline_rewards, ci = "sd",
+                    ax = ax[2], linewidth = 0.5)
+
+ax[0].set_title("Reinforce standard return per episode", fontweight = "bold")
+ax[0].set_xlabel("Episode")
+ax[0].set_ylabel("Return per episode")
+ax[0].grid()
+
+ax[1].set_title("Reinforce to-go return per episode", fontweight = "bold")
+ax[1].set_xlabel("Episode")
+ax[1].set_ylabel("Return per episode")
+ax[1].grid()
+
+ax[2].set_title("Reinforce with baseline return per episode", fontweight = "bold")
+ax[2].set_xlabel("Episode")
+ax[2].set_ylabel("Return per episode")
+ax[2].grid()
+
+fig1.tight_layout()
+
+fig1.savefig("return_per_episode_reinforces.svg")
 
 fig2 = plt.figure(figsize=(24,12))
 
